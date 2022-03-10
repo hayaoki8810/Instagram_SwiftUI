443bd27abb966baa4c34f6ff2002280c933c15b1
Hayato Aoki <81552478+Hayato-Aoki@users.noreply.github.com>
2022-03-10 13:53:27 +0900

    Initial Commit

---
+++ Instagram_SwiftUI/Instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Instagram_SwiftUI
+//
+//  Created by Hayato Aoki on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
